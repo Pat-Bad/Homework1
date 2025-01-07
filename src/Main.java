b2480222dff47e6f9d08d30ae64225d22cec1625
Patricia <patricia.badji89@gmail.com>
2025-01-07 14:29:27 +0100

    starts ex.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+
+                int x = 2;
+                int y = 5;
+                int Result = x * y;
+                System.out.println(Result);
+
+                String primaParte = "Il numero è";
+                int numeroIntero = 62;
+
+                String concatenazioneElementi = primaParte + " " + numeroIntero;
+                System.out.println(concatenazioneElementi);
+
+                //esercizio array//
+
+        String [] =
+
+    }
+}
